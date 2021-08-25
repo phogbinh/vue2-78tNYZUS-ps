55a7e20638669f825afb54c95e8a8c74f6d4177e
bill <phongbinh2511@gmail.com>
2021-08-25 18:47:54 +0800

    [master] Updated lesson 2.

--- src/components/Skills.vue
+++ src/components/Skills.vue
@@ -1,13 +1,25 @@
 <template>
   <div class="hello">
-    Skills
+    <div class='holder'>
+      <ul>
+        <li v-for='(data, index) in skills' :key='index'>{{ index }}: {{ data['skill'] }}</li>
+      </ul>
+      <p v-if='skills.length > 1'>You have more than 1 skills</p>
+      <p v-else>You have less than or equal to 1 skill</p>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
   name: 'Skills',
-  props: {
+  data() {
+    return {
+      'skills': [
+        { 'skill': 'vuejs' },
+        { 'skill': 'frontend developer' }
+      ]
+    }
   }
 }
 </script>

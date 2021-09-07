865a93cd55264365912cd934a5e00cb74a9bc235
bill <phongbinh2511@gmail.com>
2021-09-07 23:25:05 +0800

    [master] Added lesson 7.

--- src/components/Skills.vue
+++ src/components/Skills.vue
@@ -12,7 +12,9 @@
         </form>
       </ValidationObserver>
       <ul>
-        <li v-for='(data, index) in skills' :key='index'>{{ data['skill'] }}</li>
+        <transition-group enter-active-class='animated bounceInUp' leave-active-class='animated bounceOutDown'>
+          <li v-for='(data, index) in skills' :key='index'>{{ data['skill'] }}</li>
+        </transition-group>
       </ul>
       <p>These are the skills that you possess.</p>
     </div>

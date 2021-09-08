c56fd015c838dfef4c74dd28e4b7623db93fddef
bill <phongbinh2511@gmail.com>
2021-09-09 00:01:41 +0800

    [master] Added lesson 9.

--- src/components/About.vue
+++ src/components/About.vue
@@ -1,6 +1,17 @@
 <template>
   <div class='about'>
-    <h1>This is an about page</h1>
+    <h1>Hello {{ their_name }}, this is an about page</h1>
     <p>Some paragraph here.</p>
   </div>
 </template>
+
+<script>
+export default {
+  'name': 'Skills',
+  'data': function() { // did not use arrow function due to behavior of `this'
+    return {
+      'their_name': this['$route']['params']['name']
+    }
+  }
+}
+</script>
